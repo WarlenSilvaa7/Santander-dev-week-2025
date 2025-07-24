85e8e64ec97d54ac36c962ba76950618a4ac6c83
Warlen <warlensa007@gmail.com>
2025-07-24 16:40:11 -0300

    Initial commit

---
+++ src/main/java/me/dio/Santander_dev_week_2025/model/Feature.java
@@ -0,0 +1,7 @@
+package me.dio.Santander_dev_week_2025.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Feature extends BaseItem{
+}
---
+++ src/main/java/me/dio/Santander_dev_week_2025/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package me.dio.Santander_dev_week_2025.repository;
+
+import me.dio.Santander_dev_week_2025.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
